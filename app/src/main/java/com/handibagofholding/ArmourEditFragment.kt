df08640efb7f3d0a908e36113ecafbe564cb5c9a
Loonk112 <104651234+Loonk112@users.noreply.github.com>
2023-11-07 22:05:04 +0100

    v0.10.8.32 = Code:  - Added editing for item names and armour category = UI:  - Added editing view for item names and category  - Created separate UI element for armour edition and creation (currently implemented only for edition) = Firebase:

--- app/src/main/java/com/handibagofholding/ArmourEditFragment.kt
+++ app/src/main/java/com/handibagofholding/ArmourEditFragment.kt
@@ -1,32 +1,27 @@
 package com.handibagofholding
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.ktx.toObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ArmourEditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ArmourEditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var editTile: ArmourEditTile
+    lateinit var b_editCancel: Button
+    lateinit var b_editConfirm: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +29,47 @@ class ArmourEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_armour_edit, container, false)
+        val view = inflater.inflate(R.layout.fragment_armour_edit, container, false)
+
+        editTile = view.findViewById<ArmourEditTile>(R.id.editTile)
+        b_editCancel = view.findViewById<Button>(R.id.b_editCancel)
+        b_editConfirm = view.findViewById<Button>(R.id.b_editConfirm)
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ArmourEditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ArmourEditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ViewModel.db.collection("armour").document(ViewModel.item).get().addOnSuccessListener {snapshot ->
+            if (snapshot != null && snapshot.exists())
+            {
+                snapshot.toObject<ArmourData>()?.let {
+                    editTile.setValues(it)
                 }
             }
+            Log.d("ArmourEditFragment", "Success in getting data")
+        }.addOnFailureListener{
+            Log.e("ArmourEditFragment", "Error getting data", it)
+        }
+
+        b_editCancel.setOnClickListener {
+            view.findNavController().navigateUp()
+        }
+
+        b_editConfirm.setOnClickListener {
+
+            ViewModel.db.collection("armour").document(ViewModel.item).set(editTile.getValues()).addOnSuccessListener {
+                view.findNavController().navigateUp()
+                Toast.makeText(requireContext(), "Item edited.", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), "There was a problem with updating data.", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
     }
+
+
 }
--- app/src/main/java/com/handibagofholding/ItemEditFragment.kt
+++ app/src/main/java/com/handibagofholding/ItemEditFragment.kt
@@ -1,32 +1,27 @@
 package com.handibagofholding
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.firebase.firestore.ktx.toObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ItemEditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ItemEditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var et_itemName: EditText
+    lateinit var b_editCancel: Button
+    lateinit var b_editConfirm: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +29,48 @@ class ItemEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_edit, container, false)
+        val view = inflater.inflate(R.layout.fragment_item_edit, container, false)
+
+        et_itemName = view.findViewById(R.id.et_itemName)
+        b_editCancel = view.findViewById<Button>(R.id.b_editCancel)
+        b_editConfirm = view.findViewById<Button>(R.id.b_editConfirm)
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ItemEditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ItemEditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ViewModel.db.collection("items").document(ViewModel.item).get().addOnSuccessListener {snapshot ->
+            if (snapshot != null && snapshot.exists())
+            {
+                snapshot.toObject<ItemMetaData>()?.let {
+                    et_itemName.setText(it.name)
                 }
             }
+            Log.d("ItemEditFragment", "Success in getting data")
+        }.addOnFailureListener{
+            Log.e("ItemEditFragment", "Error getting data", it)
+        }
+
+        b_editCancel.setOnClickListener {
+            view.findNavController().navigateUp()
+        }
+
+        b_editConfirm.setOnClickListener {
+
+            if (et_itemName.text.isBlank()) return@setOnClickListener
+
+            val name = et_itemName.text.toString().trim()
+
+            ViewModel.db.collection("items").document(ViewModel.item).update("name",name).addOnSuccessListener {
+                view.findNavController().navigateUp()
+                Toast.makeText(requireContext(), "Item edited.", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), "There was a problem with updating data.", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
     }
 }
